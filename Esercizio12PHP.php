673faef5ad0eb942f55447046deca0e9739cf78d
Gabriel <gabrielsiano2006@gmail.com>
2024-10-03 09:56:36 +0300

    first commit

---
+++ Esercizio12PHP.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php
+
+
+function scegliColore($colore) {
+
+    switch ($colore) {
+        case 'r':
+            return 'red';
+            break;
+        case 'b':
+            return 'blue';
+            break;
+        case 'g':
+            return 'green';
+            break;
+        default:
+            return 'black';
+            break;
+    }
+
+}
+
+$coloreScelto = "b";    
+
+echo "<h1 style='color:" . scegliColore($coloreScelto) . "'>QUESTO E' IL COLORE SCELTO: " . scegliColore($coloreScelto) . "</h1>"
+
+
+
+
+
+?>
+</body>
+</html>
